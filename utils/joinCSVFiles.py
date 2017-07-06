c2c4494e1fb2abaa416623924c526e3dcd064d55
RJJishnu353 <jishnu.jayakumar182@gmail.com>
2017-07-07 03:56:12 +0530

    Sample simulation file for csvFileJoiner.py

--- utils/joinCSVFiles.py
+++ utils/joinCSVFiles.py
@@ -1,6 +1,4 @@
 from csvFileJoiner import csvFileJoiner
 import pandas as pd
 joinerObj = csvFileJoiner("../input")
-#joinerObj.joinAndGenerateCSVFiles("/home/jishnu/Documents/github/data-enrichment/output/joinedCSV","joined.csv")
-joinerObj.joinCSVFiles()
-joinerObj.generateJoinedCSVFile("/home/jishnu/Documents/github/data-enrichment/output/joinedCSV","joined.csv")
+joinerObj.joinAndGenerateCSVFiles("../output/joinedCSV","joined.csv")
