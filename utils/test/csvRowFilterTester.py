ace46ef06458c50914c5cfa7fc009370fb8f8dab
RJJishnu353 <jishnu.jayakumar182@gmail.com>
2017-07-11 00:20:33 +0530

    Removed debugging comments

--- utils/test/csvRowFilterTester.py
+++ utils/test/csvRowFilterTester.py
@@ -4,8 +4,6 @@ sys.path.append('../')
 from csvRowFilter import csvRowFilter
 
 keyColumnIds = ['NAME', 'PINCODE']
-#row_filter = csvRowFilter('../../input/jishnu_module_test/merged-self-made-test-copy.csv', keyColumnIds)
 row_filter = csvRowFilter('../../input/test/merged-test-copy.csv', keyColumnIds)
-#row_filter = csvRowFilter('../../input/jishnu_module_test/merged.csv', keyColumnIds)
 row_filter.startFiltering()
 
