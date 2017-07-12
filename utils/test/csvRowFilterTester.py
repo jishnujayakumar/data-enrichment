49a0e789147fb2e49ae2c75309412a1a71b3807f
RJJishnu353 <jishnu.jayakumar182@gmail.com>
2017-07-12 12:21:52 +0530

    Code for testing csvRowFilter.py

--- utils/test/csvRowFilterTester.py
+++ utils/test/csvRowFilterTester.py
@@ -1,12 +1,7 @@
 import sys
-<<<<<<< HEAD
 import csv
 sys.path.append('../')
 from csvRowFilter import csvRowFilter
-=======
-sys.path.append('../')
-from utils import csvRowFilter
->>>>>>> c79ffbbfc7c787be306e086f3c164dc776787664
 
 keyColumnIds = ['NAME', 'PINCODE']
 row_filter = csvRowFilter('../../input/test/merged-test-copy.csv', keyColumnIds)
