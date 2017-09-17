c337aaa737ca0d67cf4d31a5207857a3b573c03e
RJJishnu353 <jishnu.jayakumar182@gmail.com>
2017-09-17 12:39:57 +0530

    Deleted utils/test directory and updated inst-techer data of Diu and Daman to original one

--- utils/test/serializePythonToPHPTester.py
+++
@@ -1,8 +0,0 @@
-import sys
-sys.path.append('../')
-from serializePythonToPHP import serializePythonToPHP
-
-keyColumnIds = ['EduID']
-
-serilizeP2PObj = serializePythonToPHP('../../input/institute_teacher_data/__Daman_and_Diu.csv', keyColumnIds)
-#serilizeP2PObj.startSerializing( keyColumnIds)
